9006e32ec347893142484f0168825a2774c13ea7
Giorgi Mosemgvdlishvili <gmosemgvdlishvili28@gmail.com>
2023-01-05 16:48:51 +0400

    update data

--- data.js
+++ data.js
@@ -174,6 +174,16 @@ export const Projects = [
         category: ['react']
     },
     {
+        id: 21,
+        name: 'refine',
+        description: 'refine app',
+        image_url: "/images/refine.png",
+        deploy_url: 'https://react-refine-app-front-page-au92d7qsy-gio-lix.vercel.app/',
+        github_url: 'https://github.com/gio-lix/react-refine-app-front-page',
+        key_techs: ['react.js', "scss",  "typescript"],
+        category: ['react']
+    },
+    {
         id: 30,
         name: 'register',
         description: 'register form',
@@ -415,8 +425,98 @@ export const Projects = [
         category: ["javascript"]
     },
     {
+        id: 231,
+        name: "Circle Navigation ",
+        description: "Circle Navigation",
+        image_url: "/images/num_46.png",
+        deploy_url: "https://teal-syrniki-bef7ea.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/menu-bar-04",
+        key_techs: ["html", "css", "JavaScript"],
+        category: ["html"]
+    },
+    {
+        id: 232,
+        name: "Checkbox ",
+        description: "custom checkbox",
+        image_url: "/images/num_47.png",
+        deploy_url: "https://63af07a3d159de0076d1822e--gleaming-unicorn-1aecd5.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/custom-checkbox",
+        key_techs: ["html", "css", "JavaScript"],
+        category: ["html"]
+    },
+    {
+        id: 233,
+        name: "Navigation",
+        description: "Navigation Menu",
+        image_url: "/images/img_43.png",
+        deploy_url: "https://63ae93684ebb5c3167586924--papaya-platypus-3daa5d.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/menu-bar-03",
+        key_techs: ["html", "css", "JavaScript"],
+        category: ["html"]
+    },
+    {
+        id: 234,
+        name: "Hover bar 02",
+        description: "Hover bar menu",
+        image_url: "/images/img_40.png",
+        deploy_url: "https://63ad46636b1d066d3950046a--sage-fenglisu-8603ae.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/menu-bar",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
+        id: 235,
+        name: "Hover bar",
+        description: "Hover bar menu",
+        image_url: "/images/img_39.png",
+        deploy_url: "https://63ad8f806dad8026fd550b1c--mellow-concha-6ab54a.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/profile-02",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
+        id: 236,
+        name: "Side Bar",
+        description: "Side Bar menu",
+        image_url: "/images/menu_38.png",
+        deploy_url: "https://heroic-piroshki-9cd656.netlify.app/#",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/side-menu",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
+        id: 237,
+        name: "users list",
+        description: "users list",
+        image_url: "/images/img_37.png",
+        deploy_url: "https://63adcd7917115e0e628cf0e2--calm-phoenix-9d858a.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/users-list",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
+        id: 238,
+        name: "navigation",
+        description: "navigation menu",
+        image_url: "/images/img_36.png",
+        deploy_url: "https://63ad63ef6f72c105a995e42c--glowing-douhua-8d8c90.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/menu-bar-02",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
+        id: 239,
+        name: "profile",
+        description: "open profile card",
+        image_url: "/images/img_35.png",
+        deploy_url: "https://63ad7ef50caebf14e41b4b74--dynamic-raindrop-af5bf1.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/profile-01",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
         id: 240,
-        name: "card",
+        name: "card 01",
         description: "card",
         image_url: "/images/img_25.png",
         deploy_url: "https://63a2ac42eb2cf3332d37018a--sensational-pastelito-3da243.netlify.app/",
@@ -425,8 +525,18 @@ export const Projects = [
         category: ["html"]
     },
     {
+        id: 249,
+        name: "Searching Input",
+        description: "Searching Input",
+        image_url: "/images/img_45.png",
+        deploy_url: "https://63aeca0359bca70074490bfb--beamish-blancmange-fe8307.netlify.app/",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/search-bar",
+        key_techs: ["html", "css"],
+        category: ["html"]
+    },
+    {
         id: 250,
-        name: "card",
+        name: "card 02",
         description: "card",
         image_url: "/images/img_23.png",
         deploy_url: "https://63a2ad8e738b443c4a5c92d3--inquisitive-quokka-af17ce.netlify.app/",
@@ -436,7 +546,7 @@ export const Projects = [
     },
     {
         id: 255,
-        name: "card",
+        name: "card 03",
         description: "card",
         image_url: "/images/img_32.png",
         deploy_url: "https://63a9ff7335a1fb64bf9f8006--exquisite-beijinho-611ae5.netlify.app/",
@@ -445,8 +555,18 @@ export const Projects = [
         category: ["html"]
     },
     {
+        id: 259,
+        name: "Side Bar",
+        description: "side bar menu",
+        image_url: "/images/img_44.png",
+        deploy_url: "https://63aeb9179266ef4755fa5375--clever-heliotrope-d61b55.netlify.app/#",
+        github_url: "https://github.com/gio-lix/javascript-html-css/tree/side-menu-02",
+        key_techs: ["html", "css", "javascript"],
+        category: ["html"]
+    },
+    {
         id: 260,
-        name: "card",
+        name: "card 04",
         description: "card",
         image_url: "/images/img_24.png",
         deploy_url: "https://javascript-html-css-zdcg.vercel.app/",
@@ -456,7 +576,7 @@ export const Projects = [
     },
     {
         id: 270,
-        name: "card",
+        name: "card 05",
         description: "card",
         image_url: "/images/img_21.png",
         deploy_url: "https://639dcb1a89c20f1a85a4ce3c--papaya-lolly-2d1f20.netlify.app/",
@@ -466,7 +586,7 @@ export const Projects = [
     },
     {
         id: 280,
-        name: "card",
+        name: "card 06",
         description: "card",
         image_url: "/images/img_22.png",
         deploy_url: "https://639e211de97dd35ea198a286--calm-belekoy-5e945b.netlify.app/",
